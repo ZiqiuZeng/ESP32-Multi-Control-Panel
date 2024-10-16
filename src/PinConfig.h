6620ff08553ed878256eb5ba8a5ea252b724e329
ZiqiuZeng <ziqiuzeng@outlook.com>
2024-10-16 13:34:18 +0800

    Pin Configuration

---
+++ src/PinConfig.h
@@ -0,0 +1,28 @@
+#ifndef PIN_CONFIG_H
+#define PIN_CONFIG_H
+
+// Define fixed pin configurations for the project using macros
+#define EN_AUTO 3
+#define BOOT_AUTO 25
+
+#define POWER_CONTROL_PIN 6
+
+#define RED_LED_PIN 10
+#define BLUE_LED_PIN 11
+
+#define SPEED_UP_PIN 12
+#define SPEED_DOWN_PIN 13
+
+#define UP_BUTTOM_PIN 30
+#define DOWN_BUTTON_PIN 28
+#define LEFT_BUTTON_PIN 27
+#define RIGHT_BUTTON_PIN 31
+#define CONFIRM_BUTTON_PIN 29
+
+#define RXD 34
+#define TXD 35
+
+#define SDA_OUT 33
+#define SCL_OUT 36
+
+#endif
