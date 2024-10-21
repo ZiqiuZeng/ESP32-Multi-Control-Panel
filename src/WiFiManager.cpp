f72859a662118a8d733d9e4ba21fc33f93d8a8e7
ZiqiuZeng <ziqiuzeng@outlook.com>
2024-10-21 17:09:34 +0800

    Update WiFi module

--- src/WiFiManager.cpp
+++ src/WiFiManager.cpp
@@ -2,104 +2,178 @@
 #include <WiFi.h>
 #include <Preferences.h>
 
-Preferences preferences;  // Create a Preferences object
+Preferences preferences; // Create Non-Volatile Memory
 
 // Load stored default credentials from non-volatile memory
-WiFiManager::WiFiManager() {
-    loadDefaultCredentials();
+WiFiManager::WiFiManager()
+{
+    loadStoredCredentials();
 }
 
 // Set ESP32 as station
-void WiFiManager::init() {
-    WiFi.mode(WIFI_STA);  
+void WiFiManager::init()
+{
+    WiFi.mode(WIFI_STA);
 }
 
-// Scan accessible networks
-void WiFiManager::scanNetworks() {
-    int n = WiFi.scanNetworks();
-    Serial.println("Scanning... Please Wait...");
-    if (n == 0) {
-        Serial.println("No networks found");
-    } else {
-        for (int i = 0; i < n; i++) {
-            Serial.printf("%d: %s (%d dBm)\n", i + 1, WiFi.SSID(i).c_str(), WiFi.RSSI(i));
+/*
+============================================================
+    CREDENTIALS MANAGEMENT SECTION
+============================================================
+*/
+
+// Save credentials to non-volatile memory
+void WiFiManager::saveCredentials()
+{
+    preferences.begin("wifi-creds", false);
+    preferences.putInt("cred_count", credentials.size());
+
+    int i = 0;
+    for (const auto &pair : credentials)
+    {
+        preferences.putString(("ssid_" + String(i)).c_str(), pair.first);
+        preferences.putString(("password_" + String(i)).c_str(), pair.second);
+        i++;
+    }
+
+    preferences.end();
+}
+
+// Load credentials from non-volatile memory
+void WiFiManager::loadStoredCredentials()
+{
+    preferences.begin("wifi-creds", true);
+    int credentialCount = preferences.getInt("cred_count", 0);
+
+    for (int i = 0; i < credentialCount; i++)
+    {
+        String ssid = preferences.getString(("ssid_" + String(i)).c_str());
+        String password = preferences.getString(("password_" + String(i)).c_str());
+        credentials[ssid] = password;
+    }
+
+    preferences.end();
+}
+
+// Add new WiFi credentials
+void WiFiManager::addWiFiCredential(const char *ssid, const char *password)
+{
+    credentials[ssid] = password;
+    Serial.printf("Added WiFi credentials for SSID: %s", ssid);
+}
+
+// Remove WiFi Credentials
+void WiFiManager::removeWiFiCredential(const char *ssid)
+{
+    if (credentials.erase(ssid))
+    {
+        Serial.printf("Removed WiFi credentials for SSID: %s\n", ssid);
+    }
+    else
+    {
+        Serial.printf("SSID: %s not found in stored credentials.\n", ssid);
+    }
+}
+
+// List all stored WiFi Credentials
+void WiFiManager::listStoredCredentials()
+{
+    Serial.println("Stored WiFi credentials:");
+    int i;
+    for (const auto &pair : credentials)
+    {
+        Serial.printf("%d: SSID: %s\n", i + 1, pair.first.c_str());
+        i++;
+    }
+}
+
+/*
+============================================================
+    WIFI CONNECTION SECTION
+============================================================
+*/
+
+// Check if there are any available WiFi networks
+bool WiFiManager::isWiFiAvailable()
+{
+    return WiFi.scanNetworks() > 0;
+}
+
+// Try connecting to stored WiFi credentials in order of signal strength
+void WiFiManager::connectToStoredWiFi()
+{
+    scanForNetworks();
+
+    for (const auto &available : availables)
+    {
+        auto it = credentials.find(available.ssid);
+        if (it != credentials.end())
+        {
+            const String &password = it->second;
+            Serial.printf("Attempting to connect to %s\n", available.ssid.c_str());
+            connectToSelectedWiFi(available.ssid, password);
         }
     }
 }
 
-// Connect ESP32 to the selected network
-void WiFiManager::connectToNetwork(int selectedNetwork) {
-    this->selectedNetwork = selectedNetwork;
-    WiFi.begin(WiFi.SSID(selectedNetwork).c_str());
-
+void WiFiManager::connectToSelectedWiFi(const String ssid, const String password)
+{
+    WiFi.begin(ssid.c_str(), password.c_str());
     int retryCount = 0;
-    while (WiFi.status() != WL_CONNECTED && retryCount < 5) {
+
+    while (WiFi.status() != WL_CONNECTED && retryCount < 5)
+    {
         delay(1000);
         retryCount++;
-        Serial.printf("Attempting to connect... %d\n", retryCount);
+        Serial.printf("Connecting to %s, attempt %d...\n", ssid.c_str(), retryCount);
     }
 
-    if (WiFi.status() == WL_CONNECTED) {
-        Serial.println("Successfully connected to WiFi");
-        Serial.println(WiFi.localIP());
-    } else {
-        Serial.println("Failed to connect");
-    }
+    Serial.printf("Connection to %s failed :(");
 }
 
-// Connect ESP32 to default network
-void WiFiManager::connectToDefault() {
-    WiFi.begin(defaultSSID, defaultPassword);
+// Scan accessible networks
+void WiFiManager::scanForNetworks()
+{
+    Serial.println("Scanning starts... Please Wait...");
 
-    int retryCount = 0;
-    while (WiFi.status() != WL_CONNECTED && retryCount < 5) {
-        delay(1000);
-        retryCount++;
-        Serial.printf("Connecting to default network... %d\n", retryCount);
+    availables.clear(); // Clear previous scan results
+
+    int networkCount = WiFi.scanNetworks();
+
+    if (!isWiFiAvailable)
+    {
+        Serial.println("No Networks Found :(");
     }
 
-    if (WiFi.status() == WL_CONNECTED) {
-        Serial.println("Successfully connected to default WiFi");
-        Serial.println(WiFi.localIP());
-    } else {
-        Serial.println("Failed to connect to default WiFi");
+    for (int i; i < networkCount; i++)
+    {
+        AvailableWiFi network = {WiFi.SSID(i),
+                                 WiFi.RSSI(i)};
+        availables.push_back(network);
     }
+
+    // Sort by signal strength (RSSI), strongest first
+    std::sort(availables.begin(), availables.end(), [](const AvailableWiFi &a, const AvailableWiFi &b)
+              { return a.rssi > b.rssi; });
 }
 
 // Detect Wifi status
-bool WiFiManager::isConnected() {
+bool WiFiManager::isConnected()
+{
     return WiFi.status() == WL_CONNECTED;
 }
 
 // Reconnect to WiFi
-void WiFiManager::reconnect() {
-    if (!isConnected()) {
-        WiFi.reconnect();
+void WiFiManager::reconnect()
+{
+    if (!isConnected())
+    {
+        connectToStoredWiFi();
     }
 }
 
 // Disconnect to WiFi
-void WiFiManager::disconnect() {
+void WiFiManager::disconnect()
+{
     WiFi.disconnect();
-}
-
-// Load default credentials from non-volatile storage
-void WiFiManager::loadDefaultCredentials() {
-    preferences.begin("wifi-creds", true);  // Open preferences in read-only mode
-    defaultSSID = preferences.getString("ssid", "default_ssid").c_str();
-    defaultPassword = preferences.getString("password", "default_password").c_str();
-    preferences.end();
-}
-
-// Save new default credentials to non-volatile storage
-void WiFiManager::saveDefaultCredentials(const char* ssid, const char* password) {
-    preferences.begin("wifi-creds", false);  // Open preferences in read-write mode
-    preferences.putString("ssid", ssid);
-    preferences.putString("password", password);
-    preferences.end();
-    
-    // Update default credentials in memory
-    defaultSSID = ssid;
-    defaultPassword = password;
-    Serial.println("Default WiFi credentials updated");
 }
--- src/WiFiManager.h
+++ src/WiFiManager.h
@@ -1,24 +1,48 @@
 #ifndef WIFI_MANAGER_H
 #define WIFI_MANAGER_H
 
-class WiFiManager {
+#include <vector>
+#include <map>
+
+struct AvailableWiFi
+{
+    String ssid;
+    int rssi; // Signal strength
+};
+
+/**
+ * WiFiManager handles WiFi operations such as connecting, disconnecting, and scanning for networks.
+ * It uses FreeRTOS tasks to perform WiFi operations without blocking the main loop.
+ */
+class WiFiManager
+{
 public:
-    WiFiManager();  // Constructor with default credentials loaded from non-volatile storage
-    void init();  // Initialize WiFi
-    void scanNetworks();  // Scan for available networks
-    void connectToNetwork(int selectedNetwork);  // Connect to selected WiFi network
-    bool isConnected();  // Check if the device is connected to WiFi
-    void reconnect();  // Reconnect to WiFi if the connection is lost
+    WiFiManager(); // Constructor with default credentials loaded from non-volatile storage
+
+    void init();                                                          // Initialize WiFi
+    bool isWiFiAvailable();                                               // Check if WiFi available
+    void connectToStoredWiFi();                                           // Connect to stored WiFi credentials
+    void connectToSelectedWiFi(const String ssid, const String password); // Connect to selected WiFi network
+
+    void scanForNetworks();     // Scan for available networks
+    void promptForWiFiConfig(); // Prompt user to add WiFi credentials
+
+    bool isConnected(); // Check if the device is connected to WiFi
+    void reconnect();   // Reconnect to WiFi if the connection is lost
     void disconnect();  // Disconnect from WiFi
-    void connectToDefault();  // Connect to the default WiFi network
-    void loadDefaultCredentials();  // Load stored default credentials
-    void saveDefaultCredentials(const char* ssid, const char* password);  // Save credentials to non-volatile storage
-    
+
+    void listStoredCredentials();                                   // List all stored WiFi credentials
+    void addWiFiCredential(const char *ssid, const char *password); // Add new WiFi credentials
+    void removeWiFiCredential(const char *ssid);
+
 private:
-    const char* defaultSSID;      // Default WiFi SSID
-    const char* defaultPassword;  // Default WiFi password
-    int selectedNetwork;          // Store the selected network
+    std::map<String, String> credentials;
+    std::vector<AvailableWiFi> availables;
+
+    int currentCredentialIndex; // Track current credential being attempted
+
+    void loadStoredCredentials(); // Load credentials from non-volatile storage
+    void saveCredentials();       // Save all credentials to non-volatile storage
 };
 
 #endif
-
